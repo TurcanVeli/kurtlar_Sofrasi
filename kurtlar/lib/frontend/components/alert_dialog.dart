d43d50872dc32d4bcaea80f61ddd5c9ef4c2cbb1
CarpeDiem0110 <eEren_Ceylan@hotmail.com>
2022-12-22 23:00:05 +0300

    Alert Dıalog eklendı

---
+++ kurtlar/lib/frontend/components/alert_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:kurtlar/frontend/pages/registerview.dart';
+
+// NAVIGATION SU ANLIK REGISTER SAYFASINI GOSTERIYOR DEGISTIRILECEK
+
+class alertDialog extends StatelessWidget {
+  alertDialog(
+      {super.key,
+      required this.header,
+      required this.explanation,
+      required this.where});
+  String header;
+  String explanation;
+  Widget where;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(header),
+      content: Text(explanation),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => registerPage()),
+              );
+            },
+            child: Text(
+              'TAMAM',
+              style: TextStyle(color: Colors.redAccent),
+            ))
+      ],
+    );
+  }
+}
