7f53d97f3851bf0e73ea9d2f5e11607f221bd070
ErdemYavuz5 <erdemyavuz.hacisoftaoglu@gmail.com>
2022-12-30 01:53:03 +0300

    Update night.view.dart
    nightView

--- kurtlar/lib/frontend/pages/night_view.dart
+++ kurtlar/lib/frontend/pages/night_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import '../../backend/lang/language_constant.dart';
+import '../components/button.dart';
 
 class nightPage extends StatefulWidget {
-  
-
   @override
   State<nightPage> createState() => _nightPageState();
 }
@@ -12,7 +12,48 @@ class _nightPageState extends State<nightPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(child: Text("Gece Başladı",style: TextStyle(color: Colors.white),)),
+      body: SafeArea(
+          child: Center(
+        child: Center(
+            child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 1000,
+                height: 40,
+                alignment: Alignment.center,
+                child: Text(
+                  "GECE BAŞLIYOR",
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  width: 300,
+                  height: 200,
+                  child: Text(
+                    "Bütün oyuncular bir birine bakacak şekilde büyük bir çember halinde oturmalıdır. Cihaz oyuncular arasında paslanacak."
+                    "Her oyuncu derin devlete veya mafyaya ait bir rol alacak. bu rolü kimseye söyleme."
+                    "Her oyuncu rol aldıktan sonra ilk gece başlayacak.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              Button(
+                buttonText: translate(context).start,
+                where: nightPage(),
+                Height: 40,
+                Width: 375,
+                fontSize: 25,
+              ),
+            ],
+          ),
+        )),
+      )),
     );
   }
 }
